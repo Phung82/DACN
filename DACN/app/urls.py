52e70b3ec6d4d8370703f8c9affcd132f8f47082
Tiểu Phụng <63807009+Phung82@users.noreply.github.com>
2021-04-04 06:37:00 +0700

    lan 2

--- DACN/app/urls.py
+++ DACN/app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('gioithieu', views._about, name='abouttt'),
+    path('chandoan', views._chandoan, name='chandoannn'),
+    path('lienhe', views._lienhe, name='lienheee'),
 ]
--- DACN/app/views.py
+++ DACN/app/views.py
@@ -9,4 +9,13 @@ def index(request):
     #return HttpResponse("Hello, world. You're at the polls index.")
     return render(request,'pages/index.html')
 
+def _about(request):
+    return render(request,'pages/about.html')
+
+def _chandoan(request):
+    return render(request,'pages/chandoan.html')
+
+def _lienhe(request):
+    return render(request,'pages/contact.html')
+
 
